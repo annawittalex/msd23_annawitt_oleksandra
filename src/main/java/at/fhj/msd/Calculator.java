b1bb40994889d4b527a6c8868dba41106539185e
Oleksandra Annawitt <oleksandra.annawitt@edu.fh-joanneum.at>
2024-06-06 10:28:48 +0200

    factorial() method written, testCases all successful

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -19,8 +19,17 @@ public class Calculator {
     }
 
     public int factorial(int number){
-        return 0;
+        int result=1;
+        if (number<0){
+            return 0;
+        }
+        if (number==0){
+            return 1;
+        }
+
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
     }
-
-
 }
