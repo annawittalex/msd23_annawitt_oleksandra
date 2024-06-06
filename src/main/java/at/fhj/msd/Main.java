22f1e0ad10319c443f96b17d17a887fe214ef390
Oleksandra Annawitt <oleksandra.annawitt@edu.fh-joanneum.at>
2024-06-06 11:32:57 +0200

    Logging Object created in Main, screenshot ex5_2.png added

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,10 +1,17 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class Main {
-
-
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
+
+        logger.info("It is a info logger.");
+        logger.error("It is an error logger.");
+
         Calculator calculator = new Calculator();
         System.out.println("Add: "+calculator.add(28, 2));
         System.out.println("Distract: "+calculator.minus(28, 2));
